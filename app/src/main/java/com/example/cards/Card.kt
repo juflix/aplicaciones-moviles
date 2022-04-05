7e3fa71e928356b38eb65d1eb0b2cd1152a1ed61
Ju <felixjuliette@gmail.com>
2022-04-05 19:37:10 +0200

    unit 5

---
+++ app/src/main/java/com/example/cards/Card.kt
@@ -0,0 +1,113 @@
+package com.example.cards
+
+import java.lang.Double.max
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+
+open class Card(
+    var question: String,
+    var answer: String,
+    var quality: Int = -1,
+
+    var repetitions: Int = 0,
+    var interval: Long = 1L,
+    var easiness: Double = 2.5,
+
+    var creationDate: String = LocalDateTime.now().toString(),
+    var nextPracticeDate: String = LocalDateTime.now().toString(),
+    var id: String = UUID.randomUUID().toString()
+) {
+
+    override fun toString(): String = run {
+        return ("card | $question | $answer | $creationDate | $easiness | $interval | $repetitions | $nextPracticeDate")
+    }
+
+    open fun show() {
+        print("  $question \n  (press any key to see the answer)")
+        var entry = readLine()
+
+        print("  $answer\n  0 -> Difficult 3 -> Doubt 5 -> Easy :")
+        var difficulty = readLine()?.toIntOrNull() ?: -1
+
+        while (difficulty != 0 && difficulty != 3 && difficulty != 5) {
+            print(
+                "  Incorrect input, please try again.\n" +
+                        "  0 -> Difficult 3 -> Doubt 5 -> Easy :"
+            )
+            difficulty = readLine()?.toIntOrNull() ?: -1
+        }
+        quality = difficulty
+    }
+
+    fun update(currentDate: LocalDateTime) {
+        easiness = max(1.3, easiness + 0.1 - (5 - quality) * (0.08 + (5 - quality) * 0.02))
+        repetitions =
+            if (quality < 3) 0
+            else repetitions + 1
+        interval =
+            if (repetitions <= 1) 1
+            else if (repetitions == 2) 6
+            else (interval * easiness).toLong()
+
+        val date = currentDate.plusDays(interval)
+        nextPracticeDate = date.toString()
+    }
+
+    fun details() {
+        println(
+            "  easiness : ${"%.2f".format(easiness)}\trepetitions : $repetitions" +
+                    "\tinterval : $interval\tdate : $nextPracticeDate\n"
+        )
+    }
+
+    fun simulate(period: Long) {
+        var now = LocalDateTime.now()
+
+        val inputFormat = DateTimeFormatter.ISO_DATE_TIME
+        val outputFormat = DateTimeFormatter.ISO_LOCAL_DATE
+
+        for (i in 1..period) {
+
+            println(now.format(outputFormat))
+
+            val nextPD = (LocalDateTime.parse(nextPracticeDate, inputFormat)).format(outputFormat)
+
+            if (nextPD.compareTo(now.format(outputFormat)) == 0) {
+                show()
+                update(now)
+                details()
+            }
+            now = now.plusDays(1)
+        }
+    }
+}
+
+/*
+
+fun fromString(cad: String): Card {
+    var card: Card
+    var chunks = cad.split(" | ")
+    chunks.forEach {
+        it.apply {
+            trim()
+            trimEnd()
+        }
+    }
+    if (chunks[0] == "cloze") card = Cloze(chunks[1], chunks[2])
+    else if (chunks[0] == "card") card = Card(chunks[1], chunks[2])
+    else {
+        throw(Exception("Incorrect type for card in file : '${chunks[0]}'"))
+    }
+    card.apply {
+        creationDate = chunks[3]
+        easiness = chunks[4].toDouble()
+        interval = chunks[5].toLong()
+        repetitions = chunks[6].toInt()
+        nextPracticeDate = chunks[7]
+    }
+    return card
+}
+
+*/
--- app/src/main/java/com/example/cards/MainActivity.kt
+++ app/src/main/java/com/example/cards/MainActivity.kt
@@ -4,17 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
+import java.time.LocalDateTime
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setContentView(R.layout.activity_second)
 
-        val name = resources.getString(R.string.app_name)
+        val checkAnswerButton: Button = findViewById(R.id.check_answer_button)
+        val separator_view: View = findViewById(R.id.separator_view)
+        val answerTextView: TextView = findViewById(R.id.answer_text_view)
+        val difficultyButtons: LinearLayout = findViewById(R.id.difficulty_buttons)
 
-        Toast.makeText(this, name, Toast.LENGTH_LONG).show()
+        checkAnswerButton.setOnClickListener {
+            checkAnswerButton.visibility = View.INVISIBLE
+            answerTextView.visibility = View.VISIBLE
+            separator_view.visibility = View.VISIBLE
+            difficultyButtons.visibility = View.VISIBLE
+        }
+
+        var card = Card("Tree", "Árbol")
+
+        var easy_button: Button = findViewById(R.id.easy_button)
+        var doubt_button: Button = findViewById(R.id.doubt_button)
+        var difficult_button: Button = findViewById(R.id.difficult_button)
+
+        easy_button.setOnClickListener {
+            card.quality = 5
+            card.update(LocalDateTime.now())
+            Toast.makeText(applicationContext, String.format("%.1f", card.easiness), Toast.LENGTH_SHORT).show()
+        }
+        doubt_button.setOnClickListener {
+            card.quality = 3
+            card.update(LocalDateTime.now())
+            Toast.makeText(applicationContext, String.format("%.1f", card.easiness), Toast.LENGTH_SHORT).show()
+        }
+        difficult_button.setOnClickListener {
+            card.quality = 0
+            card.update(LocalDateTime.now())
+            Toast.makeText(applicationContext, String.format("%.1f", card.easiness), Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 }
