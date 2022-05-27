70cf23a9c53bc141df9c99853d95f0be41657110
felix <felixjuliette@gmail.com>
2022-05-27 18:44:34 +0200

    unit 23 done

--- app/src/main/java/com/example/cards/CardAdapter.kt
+++ app/src/main/java/com/example/cards/CardAdapter.kt
@@ -1,18 +1,36 @@
 package com.example.cards
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class CardAdapter : RecyclerView.Adapter<CardAdapter.CardHolder>() {
 
     var data =  listOf<Card>()
 
-    class CardHolder(view: View) : RecyclerView.ViewHolder(view)
+    class CardHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val questionTextView: TextView = itemView.findViewById(R.id.list_item_question)
+        private val answerTextView:TextView = itemView.findViewById(R.id.list_item_answer)
+        private val dateTextView: TextView = itemView.findViewById(R.id.list_item_date)
+
+        fun bind(card: Card){
+            questionTextView.text = card.question
+            answerTextView.text = card.answer
+            dateTextView.text = card.creationDate.substring(0,10)
+
+            itemView.setOnClickListener {
+                Toast.makeText(it.context, card.question, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardHolder {
-        return CardHolder(TextView(parent.context))
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_card, parent, false)
+        return CardHolder(view)
     }
 
     override fun getItemCount() = data.size
@@ -21,7 +39,6 @@ class CardAdapter : RecyclerView.Adapter<CardAdapter.CardHolder>() {
         holder: CardHolder,
         position: Int
     ) {
-        val item = data[position]
-        (holder.itemView as TextView).text = "${item.question} \n ${item.answer}"
+        holder.bind(data[position])
     }
 }
